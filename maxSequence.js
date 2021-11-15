6440374272b4437181c8915ebd033d386487db8d
leoff00 <leof0180@gmail.com>
2021-11-14 21:41:42 -0300

    need to refactor

--- maxSequence.js
+++ maxSequence.js
@@ -1,12 +1,14 @@
 const maxSequence = (arr) => {
-  if (arr === []) return 0;
+  if (arr === [] || arr.length === 0) return 0;
 
   const removeDuplicateNumber = arr
     .sort((a, b) => a - b)
-    .filter((e, i, a) => a[i] !== a[i + 1]);
-  const removeNegativeNumbers = removeDuplicateNumber.filter((e) => e > 1);
+    .filter((e, i, a) => (e > 0 ? a[i] !== a[i + 1] : e));
 
+  const removeNegativeNumbers = removeDuplicateNumber.filter((e) => e > 1);
   return removeNegativeNumbers.reduce((acc, element) => acc + element, 0);
 };
 
-console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])); //output: 6
+console.log(maxSequence([5, 2, 1, -2]));
+
+// TODO: Refactor
